cda0a6fa42461f984acbf8d8cd943a3b2c204fd9
joe20182 <joe790924@gmail.com>
2022-03-15 23:31:23 +0800

    edit: Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,27 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
+import LogoImg from '@/assets/img/logo.png'
+import { Logo, HeaderWrapper, NavWrapper, NavItem } from './style'
+import Hamburger from './Hambergur'
 
 const Header: FC = () => {
-  return <div className="header">HEADER</div>
+  const [hamToggled, setHamToggled] = useState(false)
+
+  const handleToggle = () => {
+    setHamToggled(!hamToggled)
+  }
+
+  return (
+    <HeaderWrapper className="header">
+      <Logo src={LogoImg} alt="" />
+      <NavWrapper className={hamToggled ? 'toggled' : ''}>
+        <NavItem>THE CLUB</NavItem>
+        <NavItem>THE TEAM</NavItem>
+        <NavItem>ROADMAP</NavItem>
+        <NavItem>MINT</NavItem>
+      </NavWrapper>
+      <Hamburger clickEvent={handleToggle} />
+    </HeaderWrapper>
+  )
 }
 
 export default Header
--- src/style/rwd.ts
+++ src/style/rwd.ts
@@ -1,10 +1,13 @@
 const breakpoint = {
-  sm: '576px', // 直平板
+  sm: '767px', // 直平板
   md: '768px', // 橫平板
   lg: '992px',
   xl: '1200px', // PC
 }
 
 export const rwd = (size: keyof typeof breakpoint) => (content: string) => {
+  if (size === 'sm') {
+    return `@media screen and (max-width: ${breakpoint[size]} ) {${content}}`
+  }
   return `@media screen and (min-width: ${breakpoint[size]} ) {${content}}`
 }
